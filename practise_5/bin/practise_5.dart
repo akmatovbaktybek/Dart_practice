559a6c1726e2c9e5deef40249894cea490dd14a3
akmatovbaktybek <byakmatov@gmail.com>
2022-12-13 15:29:13 +0600

    practise_5

---
+++ practise_5/bin/practise_5.dart
@@ -0,0 +1,69 @@
+import 'dart:math';
+
+void main() {
+  // 1 задание
+  getNumSquare(int a) {
+    return pow(a, 2);
+  }
+
+  var numSquare = getNumSquare(4);
+  print('Квадрат чиcла: $numSquare');
+
+  // 2 задание
+  getSumOfNum(int num1, int num2) {
+    int sumNum = num1 + num2;
+    return sumNum;
+  }
+
+  var sumOfNum = getSumOfNum(10, 40);
+  print('Сумма чисел: $sumOfNum');
+
+  // 3 задание
+
+  getResultOfExpression(int a1, int b1, int c1) {
+    var result = (a1 - b1) / c1;
+    return result;
+  }
+
+  var resultOfExpression = getResultOfExpression(16, 7, 4);
+  print('Результат выражения $resultOfExpression');
+
+  // 5 задание
+  getMinuteToSec(int minute) {
+    var minuteToSec = minute * 60;
+    return minuteToSec;
+  }
+
+  var seconds = getMinuteToSec(5);
+  print('Результат перевода в секунды: $seconds секунд');
+
+  // 6 задание
+  List array = [1, 3, 4, 5, 6];
+  getListsFirstElement(list) {
+    return list[0];
+  }
+
+  var firstElem = getListsFirstElement(array);
+
+  print('Первый элемент массива: $firstElem');
+
+  // 7 задание
+    bool boolValue = false;
+    if (boolValue == true) {
+      print('Ваша переменная имеет значение: $boolValue');
+    } else {
+      print('Ваша переменная имеет значение: $boolValue');
+    }
+
+    // 8 задание
+    showValue(var number) {
+      if(number <= 0) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    var value = showValue(-34);
+    print('Результат: $value');
+}
---
+++ practise_5/test/practise_5_test.dart
@@ -0,0 +1,8 @@
+import 'package:practise_5/practise_5.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
